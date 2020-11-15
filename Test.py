a35a9db87bb464999142735e82827b996d70a744
Vikrant Sharma <iamvikrant1@gmail.com>
2020-11-15 22:41:50 +0530

    .

--- Test.py
+++ Test.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.binary_location = "C:/Program Files (x86)/BraveSoftware/Brave-Browser/Application/brave.exe"
 PATH = "C:/Program Files (x86)/chromedriver.exe"
-driver = webdriver.Chrome(chrome_options=options,executable_path=PATH)
+driver = webdriver.Chrome(options=options,executable_path=PATH)
 
 driver.get("https://techwithtim.net")
+print(driver.title)
+driver.quit()
